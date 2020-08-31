85730ec933d84713654b426c217705a24d769c2e
fkaraagac11 <fehmi11.qa@gmail.com>
2020-08-30 22:55:20 -0400

    update"

--- ReactJS-Udemy/Basic/spread_operator.js
+++ ReactJS-Udemy/Basic/spread_operator.js
@@ -1,3 +1,4 @@
+//SPREAD OPERATOR
 const x = [1, 2, 3];
 const y = [...x, 4];
 console.log(y);
@@ -6,3 +7,16 @@ const person = { name: "Fehmi" };
 const newPerson = { ...person, age: 22 };
 
 console.log(newPerson);
+
+const filter = (...args) => {
+    return args.filter((el) => el === 1);
+};
+
+console.log(filter(1, 2, 3));
+
+// MAP FUNCTION
+const x = [1, 2, 3];
+
+const y = x.map((NUM) => {
+    console.log(NUM * 3);
+});
