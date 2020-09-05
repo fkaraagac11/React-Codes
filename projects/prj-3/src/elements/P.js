90c2509e2779a4cacb9b0d58fb1620fbd75a97eb
fkaraagac11 <fehmi11.qa@gmail.com>
2020-09-04 22:18:18 -0400

    update

--- projects/prj-3/src/elements/P.js
+++ projects/prj-3/src/elements/P.js
@@ -1,14 +1,14 @@
 import styled, { css } from "styled-components";
 
 const P = styled.p`
-    color: brown;
+    color: red;
     font-size: 22px;
     border: 5px dashed red;
     margin-left: 20px;
     margin-right: 20px;
     margin-top: 200px;
     padding: 10px 20px;
-    font: sans-serif;
+    font-family: "Times New Roman", Times, serif;
 `;
 
 export default P;
